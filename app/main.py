a3a25b778838f6a603cc7a270bcd39c5fd101a16
zyambo <mightypz2@gmail.com>
2026-01-06 11:49:05 +0200

    chore: Fix formatting

--- app/main.py
+++ app/main.py
@@ -3,18 +3,23 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from app.routes import router
 
-app = FastAPI(title="Zambia Geo API", description="API for Zambian Geographic Data")
+app = FastAPI(
+    title="Zambia Geo API", description="API for Zambian Geographic Data")
 
 # Mount the static files directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 # Serve the index.html at the root
 @app.get("/", include_in_schema=False)
 async def serve_home():
+    """Endpoint serves the static landing page for non technial users"""
     return FileResponse("static/index.html")
+
 
 app.include_router(router, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- app/services.py
+++ app/services.py
@@ -1,7 +1,9 @@
 from zambia_geo import get_all_provinces, get_province_cities
 
+
 def list_provinces():
     return get_all_provinces()
+
 
 def fetch_cities_by_province(province_name: str):
     # Standardize input for validation
@@ -9,6 +11,7 @@ def fetch_cities_by_province(province_name: str):
     if province_name.lower() not in provinces:
         return None
     return get_province_cities(province_name)
+
 
 def search_cities_globally(query: str):
     results = []
